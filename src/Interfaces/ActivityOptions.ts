64646c70f84714314357210d12ad37b7ed7cb313
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-09-08 09:24:25 -0400

    jsDoc for ActivityOptions.ts

--- src/Interfaces/ActivityOptions.ts
+++ src/Interfaces/ActivityOptions.ts
@@ -1,20 +1,62 @@
 import { ACTIVITY } from "..";
 
 export interface IActivity {
-  name: string; // The activity name
-  type: ACTIVITY; // The activity type
-  created_at: number; // Timestamp of when the activity was added to the user's session
-  url?: string | null; // Stream url, is validated when type is 1
-  timestamps?: ITimestamps; // Timestamps for start and/or end of the game
-  application_id?: string; // Application id for the game
-  details?: string | null; // What the player is currently doing
-  state?: string | null; // The user's current party status
-  emoji?: IActivityEmoji | null; // The emoji used for a custom status
-  party?: IParty; // Information for the current party of the player
-  assets?: IAssets; // Images for the presence and their hover texts
-  secrets?: ISecrets; // Secrets for Rich Presence joining and spectating
-  instance?: boolean; // Whether or not the activity is an instanced game session
-  flags?: number; // Activity flags ORd together, describes what the payload includes
+  /**
+   * The activity name
+   */
+  name: string;
+  /**
+   * The activity type
+   */
+  type: ACTIVITY;
+  /**
+   * Timestamp of when the activity was added to the user's session
+   */
+  created_at: number;
+  /**
+   * Stream url, is validated when type is 1
+   */
+  url?: string | null;
+  /**
+   * Timestamps for start and/or end of the game
+   */
+  timestamps?: ITimestamps;
+  /**
+   * Application id for the game
+   */
+  application_id?: string;
+  /**
+   *  What the player is currently doing
+   */
+  details?: string | null;
+  /**
+   * The user's current party status
+   */
+  state?: string | null;
+  /**
+   * The emoji used for a custom status
+   */
+  emoji?: IActivityEmoji | null;
+  /**
+   * Information for the current party of the player
+   */
+  party?: IParty;
+  /**
+   * Images for the presence and their hover texts
+   */
+  assets?: IAssets;
+  /**
+   * Secrets for Rich Presence joining and spectating
+   */
+  secrets?: ISecrets;
+  /**
+   * Whether or not the activity is an instanced game session
+   */
+  instance?: boolean;
+  /**
+   * Activity flags ORd together, describes what the payload includes
+   */
+  flags?: number;
 }
 
 export interface ITimestamps {
@@ -23,25 +65,61 @@ export interface ITimestamps {
 }
 
 export interface IActivityEmoji {
-  name: string; // Emoji name
-  id?: string; // Emoji ID
-  animated?: boolean; // Whether the emoji is animated
+  /**
+   * Emoji name
+   */
+  name: string;
+  /**
+   * Emoji ID
+   */
+  id?: string;
+  /**
+   * Whether the emoji is animated
+   */
+  animated?: boolean;
 }
 
 export interface IParty {
-  id?: string; // The id of the party
-  size?: [number, number]; // The party's current and maximum size
+  /**
+   * The id of the party
+   */
+  id?: string;
+  /**
+  * The party's current and maximum size
+  */
+  size?: [number, number];
 }
 
 export interface IAssets {
-  large_image?: string; // The ID for a large asset of the activity
-  large_text?: string; // Text displayed when hovering over the large image of the activity
-  small_image?: string; // The ID for a small asset of the activity
-  small_text?: string; // Text displayed when hovering over the small image of the activity
+  /**
+   * The ID for a large asset of the activity
+   */
+  large_image?: string;
+  /**
+   * Text displayed when hovering over the large image of the activity
+   */
+  large_text?: string;
+  /**
+   * The ID for a small asset of the activity
+   */
+  small_image?: string;
+  /**
+   * Text displayed when hovering over the small image of the activity
+   */
+  small_text?: string;
 }
 
 export interface ISecrets {
-  join?: string; // The secret for joining a party
-  spectate?: string; // The secret for spectating a game
-  match?: string; // The secret for a specific instanced match
+  /**
+   * The secret for joining a party
+   */
+  join?: string;
+  /**
+   * The secret for spectating a game
+   */
+  spectate?: string;
+  /**
+   * The secret for a specific instanced match
+   */
+  match?: string;
 }
