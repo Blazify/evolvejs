2d272ec9bbc3fb0bba2ec4d001e4afec589ae48a
ronit <runa.ronit3129@gmail.com>
2020-09-16 23:55:08 +0530

    Builder Decorator

--- src/Decorators/Events.ts
+++ src/Decorators/Events.ts
@@ -7,6 +7,5 @@ export const listeners = new Objex<Array<string>, EvolveClient>();
 export function Event(eventName?: string) {
 	return (target: EvolveClient, propertyKey: string, propertyDescriptor: PropertyDescriptor): void => {
 		if(propertyDescriptor.writable) listeners.set([eventName ? eventName : propertyKey, propertyKey], target);
-		console.log(listeners);
 	};
 }
---
+++ src/Decorators/EvolveBuilderDecorator.ts
@@ -0,0 +1,32 @@
+import { EvolveBuilder } from "../Client/EvolveBuilder";
+import { EvolveClient } from "../Client/EvolveClient";
+import { Structures } from "../Structures/Structures";
+import { CacheOptions, GatewayIntents } from "../Utils/Constants";
+
+export function Builder(options: BuilderDecoratorOptions) {
+	return (target: typeof EvolveClient): void => {
+		const builder: EvolveBuilder = new EvolveBuilder(options.token, options.useDefaultSetting ? true : false);
+		builder.setClientClass(target);
+		if(options.intents) builder.enableIntents(...options.intents);
+		if(options.cache) builder.enableCache(...options.cache);
+		if(options.secret) builder.setSecret(options.secret);
+		if(options.activity) builder.setActivity(options.activity);
+		if(options.encoding) builder.setEncoding(options.encoding);
+		if(options.shards) builder.setShards(options.shards);
+		if(options.structure) builder.setStructureClass(options.structure);
+
+		builder.build();
+	};
+}
+
+interface BuilderDecoratorOptions {
+    intents?: GatewayIntents[];
+    cache?: CacheOptions[];
+    useDefaultSetting?: boolean;
+    token: string;
+    secret?: string;
+    activity?: Object;
+    encoding?: "json" | "etf";
+    shards?: number;
+    structure?: Structures
+}
