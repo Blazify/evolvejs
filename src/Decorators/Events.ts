9605782123167e9ed516b4f47ba64cc01c364aea
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-09-16 10:31:30 -0400

    improved event decorator

--- src/Decorators/Events.ts
+++ src/Decorators/Events.ts
@@ -2,10 +2,14 @@ import { Objex } from "@evolvejs/objex";
 import "reflect-metadata";
 import { EvolveClient } from "../Client/EvolveClient";
 
-export const listeners = new Objex<string, EvolveClient>()
+export const listeners = new Objex<string, EvolveClient>();
 
-export function Event() {
+export function Event(eventName?: string) {
 	return (target: EvolveClient, propertyKey: string, propertyDescriptor: PropertyDescriptor): void => {
-		if(propertyDescriptor) listeners.set(propertyKey, target);
+		if (propertyDescriptor) {
+			if (eventName)
+				listeners.set(eventName, target);
+			else listeners.set(propertyKey, target);
+		}
 	};
 }
