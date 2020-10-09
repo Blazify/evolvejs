7788e3c11a6d474708131dbb1229ffb14c487659
romeah <runa.ronit3129@gmail.com>
2020-10-09 11:55:05 +0000

    Added more channel endpoints

--- src/Client/API/RestAPIHandler.ts
+++ src/Client/API/RestAPIHandler.ts
@@ -4,89 +4,89 @@ import { EvolveClient, IAPIParams, CONSTANTS } from "../..";
 import { promisify } from "util";
 
 export class RestAPIHandler {
-	private _ratelimited = 0;
-	private _lastFetchReturnHeader!: Headers;
-	constructor(public client: EvolveClient) { }
+  private _ratelimited = 0;
+  private _lastFetchReturnHeader!: Headers;
+  constructor(public client: EvolveClient) {}
 
-	public async fetch(options: IAPIParams): Promise<any> {
-		try {
-			if (this._lastFetchReturnHeader) {
-				const remaining = this._lastFetchReturnHeader.get(
-					"X-RateLimit-Remaining"
-				);
-				let resetAfter: unknown = this._lastFetchReturnHeader.get(
-					"X-RateLimit-Reset-After"
-				);
-				if (resetAfter) resetAfter = Number(resetAfter) * 1000;
-				if (remaining == "0") {
-					await promisify(setTimeout)((resetAfter as unknown) as number);
-				}
-			}
-			if (options.method !== "POST" && options.method !== "PATCH") {
-				const fetched = await fetch(`${CONSTANTS.Api}/${options.endpoint}`, {
-					method: options.method,
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: `Bot ${this.client.token}`,
-					},
-				});
+  public async fetch(options: IAPIParams): Promise<any> {
+  	try {
+  		if (this._lastFetchReturnHeader) {
+  			const remaining = this._lastFetchReturnHeader.get(
+  				"X-RateLimit-Remaining"
+  			);
+  			let resetAfter: unknown = this._lastFetchReturnHeader.get(
+  				"X-RateLimit-Reset-After"
+  			);
+  			if (resetAfter) resetAfter = Number(resetAfter) * 1000;
+  			if (remaining == "0") {
+  				await promisify(setTimeout)((resetAfter as unknown) as number);
+  			}
+  		}
+  		if (options.method !== "POST" && options.method !== "PATCH") {
+  			const fetched = await fetch(`${CONSTANTS.Api}/${options.endpoint}`, {
+  				method: options.method,
+  				headers: {
+  					"Content-Type": "application/json",
+  					Authorization: `Bot ${this.client.token}`,
+  				},
+  			});
 
-				if (fetched.status === 429) {
-					const json = await fetched.json();
-					this.client.logger.warn(
-						`Rate Limited. Reason: ${json.body}, Global: ${json.global}\n Don't Worry, your request will be retried after ${json.retry_after}`
-					);
-					this._ratelimited += 1;
-					if (this._ratelimited === 50) {
-						this.client.sharder.destroyAll();
-					}
-					await promisify(setTimeout)(json.retry_after);
-					return this.fetch(options);
-				}
-				this._lastFetchReturnHeader = fetched.headers;
+  			if (fetched.status === 429) {
+  				const json = await fetched.json();
+  				this.client.logger.warn(
+  					`Rate Limited. Reason: ${json.body}, Global: ${json.global}\n Don't Worry, your request will be retried after ${json.retry_after}`
+  				);
+  				this._ratelimited += 1;
+  				if (this._ratelimited === 50) {
+  					this.client.sharder.destroyAll();
+  				}
+  				await promisify(setTimeout)(json.retry_after);
+  				return this.fetch(options);
+  			}
+  			this._lastFetchReturnHeader = fetched.headers;
 
-				return fetched.json();
-			} else {
-				let body;
-				if (options.postType == "Message") {
-					body = JSON.stringify(options.message);
-				} else if (options.postType == "Channel") {
-					body = JSON.stringify(options.channel);
-				} else if (options.postType == "Integration") {
-					body = JSON.stringify(options.integration);
-				}
+  			return fetched.json();
+  		} else {
+  			let body;
+  			if (options.postType == "Message") {
+  				body = JSON.stringify(options.message);
+  			} else if (options.postType == "Channel") {
+  				body = JSON.stringify(options.channel);
+  			} else if (options.postType == "Integration") {
+  				body = JSON.stringify(options.integration);
+  			}
 
-				if (!body)
-					throw this.client.logger.error("No Post Type Given in POST fetching");
+  			if (!body)
+  				throw this.client.logger.error("No Post Type Given in POST fetching");
 
-				const fetched = await fetch(`${CONSTANTS.Api}/${options.endpoint}`, {
-					method: options.method,
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: `Bot ${this.client.token}`,
-					},
-					body: body,
-				});
+  			const fetched = await fetch(`${CONSTANTS.Api}/${options.endpoint}`, {
+  				method: options.method,
+  				headers: {
+  					"Content-Type": "application/json",
+  					Authorization: `Bot ${this.client.token}`,
+  				},
+  				body: body,
+  			});
 
-				if (fetched.status === 429) {
-					const json = await fetched.json();
-					this.client.logger.warn(
-						`Rate Limited. Reason: ${json.body}, Global: ${json.global}\n Don't Worry, your request will be retried after ${json.retry_after}`
-					);
-					this._ratelimited += 1;
-					if (this._ratelimited === 50) {
-						this.client.sharder.destroyAll();
-					}
-					await promisify(setTimeout)(json.retry_after);
-					return this.fetch(options);
-				}
+  			if (fetched.status === 429) {
+  				const json = await fetched.json();
+  				this.client.logger.warn(
+  					`Rate Limited. Reason: ${json.body}, Global: ${json.global}\n Don't Worry, your request will be retried after ${json.retry_after}`
+  				);
+  				this._ratelimited += 1;
+  				if (this._ratelimited === 50) {
+  					this.client.sharder.destroyAll();
+  				}
+  				await promisify(setTimeout)(json.retry_after);
+  				return this.fetch(options);
+  			}
 
-				this._lastFetchReturnHeader = fetched.headers;
+  			this._lastFetchReturnHeader = fetched.headers;
 
-				return fetched.json();
-			}
-		} catch (e) {
-			throw this.client.logger.error(e);
-		}
-	}
+  			return fetched.json();
+  		}
+  	} catch (e) {
+  		throw this.client.logger.error(e);
+  	}
+  }
 }
