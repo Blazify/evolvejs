f6d9bb38fe557f33db231595de0e55411f0350f1
romeah <runa.ronit3129@gmail.com>
2020-10-11 03:50:16 +0530

    saber's rest api handler fix

--- src/Client/RestAPI/RestAPIHandler.ts
+++ src/Client/RestAPI/RestAPIHandler.ts
@@ -2,6 +2,8 @@
 import fetch, { Headers } from "node-fetch";
 import { EvolveClient, IAPIParams, CONSTANTS } from "../..";
 import { promisify } from "util";
+import DiscordRejection from "./DiscordRejection";
+import { EVENTS } from "../..";
 
 export class RestAPIHandler {
   private _ratelimited = 0;
@@ -22,57 +24,88 @@ export class RestAPIHandler {
           await promisify(setTimeout)((resetAfter as unknown) as number);
         }
       }
+      if (options.method !== "POST") {
+        const fetched = await fetch(`${CONSTANTS.Api}/${options.endpoint}`, {
+          method: options.method,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bot ${this.client.token}`,
+          },
+        });
 
-      let body;
-      if (options.postType == "Message") {
-        body = JSON.stringify(options.message);
-      } else if (options.postType == "Channel") {
-        body = JSON.stringify(options.channel);
-      } else if (options.postType == "Integration") {
-        body = JSON.stringify(options.integration);
-      } else if (options.postType == "[Message]") {
-        body = JSON.stringify(options.messages);
-      } else if (options.postType == "JSON") {
-        body = JSON.stringify(options.json_params);
-      }
-
-      const fetched = await fetch(
-        `${CONSTANTS.Api}/${options.endpoint}`,
-        options.method == "POST" ||
-          options.method == "PUT" ||
-          options.method == "PATCH"
-          ? {
-              method: options.method,
-              headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bot ${this.client.token}`,
-              },
-              body: body,
-            }
-          : {
-              method: options.method,
-              headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bot ${this.client.token}`,
-              },
-            }
-      );
-
-      if (fetched.status === 429) {
-        const json = await fetched.json();
-        this.client.logger.warn(
-          `Rate Limited. Reason: ${json.body}, Global: ${json.global}\n Don't Worry, your request will be retried after ${json.retry_after}`
-        );
-        this._ratelimited += 1;
-        if (this._ratelimited === 50) {
-          this.client.sharder.destroyAll();
+        if (fetched.status === 429) {
+          const json = await fetched.json();
+          this.client.logger.warn(
+            `Rate Limited. Reason: ${json.body}, Global: ${json.global}\n Don't Worry, your request will be retried after ${json.retry_after}`
+          );
+          this._ratelimited += 1;
+          if (this._ratelimited === 50) {
+            this.client.sharder.destroyAll();
+          }
+          await promisify(setTimeout)(json.retry_after);
+          return this.fetch(options);
         }
-        await promisify(setTimeout)(json.retry_after);
+        this._lastFetchReturnHeader = fetched.headers;
+
+        if (fetched.status.toString().startsWith("20")) {
+          const err = await fetched.json();
+          const rejection = new DiscordRejection({
+            code: err.code,
+            msg: err.msg,
+            http: fetched.status,
+            path: options.endpoint,
+          });
+
+          this.client.emit(EVENTS.API_ERROR, rejection);
+          if (this.client.listenerCount(EVENTS.API_ERROR) < 1) throw rejection;
+        } else return fetched.json();
+      } else {
+        let body;
+        if (options.postType == "Message") {
+          body = JSON.stringify(options.message);
+        } else if (options.postType == "Channel") {
+          body = JSON.stringify(options.channel);
+        }
+
+        if (!body)
+          throw this.client.logger.error("No Post Type Given in POST fetching");
+
+        const fetched = await fetch(`${CONSTANTS.Api}/${options.endpoint}`, {
+          method: options.method,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bot ${this.client.token}`,
+          },
+          body: body,
+        });
+
+        if (fetched.status === 429) {
+          const json = await fetched.json();
+          this.client.logger.warn(
+            `Rate Limited. Reason: ${json.body}, Global: ${json.global}\n Don't Worry, your request will be retried after ${json.retry_after}`
+          );
+          this._ratelimited += 1;
+          if (this._ratelimited === 50) {
+            this.client.sharder.destroyAll();
+          }
+          await promisify(setTimeout)(json.retry_after);
+          return this.fetch(options);
+        }
+
+        this._lastFetchReturnHeader = fetched.headers;
+        if (fetched.status.toString().startsWith("20")) {
+          const err = await fetched.json();
+          const rejection = new DiscordRejection({
+            code: err.code,
+            msg: err.msg,
+            http: fetched.status,
+            path: options.endpoint,
+          });
+
+          this.client.emit(EVENTS.API_ERROR, rejection);
+          if (this.client.listenerCount(EVENTS.API_ERROR) < 1) throw rejection;
+        } else return fetched.json();
       }
-
-      this._lastFetchReturnHeader = fetched.headers;
-
-      return fetched.json();
     } catch (e) {
       throw this.client.logger.error(e);
     }
