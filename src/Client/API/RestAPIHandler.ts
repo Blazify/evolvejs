5bfb414452c974516fc76fe9e83948d9b684afa3
saberscientist <70531349+saberscientist@users.noreply.github.com>
2020-10-11 02:44:22 +1100

    thong

--- src/Client/API/RestAPIHandler.ts
+++ src/Client/API/RestAPIHandler.ts
@@ -2,89 +2,104 @@
 import fetch, { Headers } from "node-fetch";
 import { EvolveClient, IAPIParams, CONSTANTS } from "../..";
 import { promisify } from "util";
+import DiscordRejection from "./DiscordRejection"
+import { EVENTS } from "../.."
 
 export class RestAPIHandler {
-  private _ratelimited = 0;
-  private _lastFetchReturnHeader!: Headers;
-  constructor(public client: EvolveClient) {}
+	private _ratelimited = 0;
+	private _lastFetchReturnHeader!: Headers;
+	constructor(public client: EvolveClient) { }
 
-  public async fetch(options: IAPIParams): Promise<any> {
-  	try {
-  		if (this._lastFetchReturnHeader) {
-  			const remaining = this._lastFetchReturnHeader.get(
-  				"X-RateLimit-Remaining"
-  			);
-  			let resetAfter: unknown = this._lastFetchReturnHeader.get(
-  				"X-RateLimit-Reset-After"
-  			);
-  			if (resetAfter) resetAfter = Number(resetAfter) * 1000;
-  			if (remaining == "0") {
-  				await promisify(setTimeout)((resetAfter as unknown) as number);
-  			}
-  		}
-  		if (options.method !== "POST") {
-  			const fetched = await fetch(`${CONSTANTS.Api}/${options.endpoint}`, {
-  				method: options.method,
-  				headers: {
-  					"Content-Type": "application/json",
-  					Authorization: `Bot ${this.client.token}`,
-  				},
-  			});
+	public async fetch(options: IAPIParams): Promise<any> {
+		try {
+			if (this._lastFetchReturnHeader) {
+				const remaining = this._lastFetchReturnHeader.get(
+					"X-RateLimit-Remaining"
+				);
+				let resetAfter: unknown = this._lastFetchReturnHeader.get(
+					"X-RateLimit-Reset-After"
+				);
+				if (resetAfter) resetAfter = Number(resetAfter) * 1000;
+				if (remaining == "0") {
+					await promisify(setTimeout)((resetAfter as unknown) as number);
+				}
+			}
+			if (options.method !== "POST") {
+				const fetched = await fetch(`${CONSTANTS.Api}/${options.endpoint}`, {
+					method: options.method,
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bot ${this.client.token}`,
+					},
+				});
 
-  			if (fetched.status === 429) {
-  				const json = await fetched.json();
-  				this.client.logger.warn(
-  					`Rate Limited. Reason: ${json.body}, Global: ${json.global}\n Don't Worry, your request will be retried after ${json.retry_after}`
-  				);
-  				this._ratelimited += 1;
-  				if (this._ratelimited === 50) {
-  					this.client.sharder.destroyAll();
-  				}
-  				await promisify(setTimeout)(json.retry_after);
-  				return this.fetch(options);
-  			}
-  			this._lastFetchReturnHeader = fetched.headers;
 
-  			return fetched.json();
-  		} else {
-  			let body;
-  			if (options.postType == "Message") {
-  				body = JSON.stringify(options.message);
-  			} else if (options.postType == "Channel") {
-  				body = JSON.stringify(options.channel);
-  			}
 
-  			if (!body)
-  				throw this.client.logger.error("No Post Type Given in POST fetching");
+				if (fetched.status === 429) {
+					const json = await fetched.json();
+					this.client.logger.warn(
+						`Rate Limited. Reason: ${json.body}, Global: ${json.global}\n Don't Worry, your request will be retried after ${json.retry_after}`
+					);
+					this._ratelimited += 1;
+					if (this._ratelimited === 50) {
+						this.client.sharder.destroyAll();
+					}
+					await promisify(setTimeout)(json.retry_after);
+					return this.fetch(options);
+				}
+				this._lastFetchReturnHeader = fetched.headers;
 
-  			const fetched = await fetch(`${CONSTANTS.Api}/${options.endpoint}`, {
-  				method: options.method,
-  				headers: {
-  					"Content-Type": "application/json",
-  					Authorization: `Bot ${this.client.token}`,
-  				},
-  				body: body,
-  			});
+				if (fetched.status !== 200) {
+					const err = await fetched.json()
+					const rejection = new DiscordRejection({
+						code: err.code,
+						msg: err.msg,
+						http: fetched.status,
+						path: options.endpoint
+					})
 
-  			if (fetched.status === 429) {
-  				const json = await fetched.json();
-  				this.client.logger.warn(
-  					`Rate Limited. Reason: ${json.body}, Global: ${json.global}\n Don't Worry, your request will be retried after ${json.retry_after}`
-  				);
-  				this._ratelimited += 1;
-  				if (this._ratelimited === 50) {
-  					this.client.sharder.destroyAll();
-  				}
-  				await promisify(setTimeout)(json.retry_after);
-  				return this.fetch(options);
-  			}
+					this.client.emit(EVENTS.API_ERROR, rejection)
+					if (this.client.listenerCount(EVENTS.API_ERROR) < 1) throw rejection;
+				} else return fetched.json();
+			} else {
+				let body;
+				if (options.postType == "Message") {
+					body = JSON.stringify(options.message);
+				} else if (options.postType == "Channel") {
+					body = JSON.stringify(options.channel);
+				}
 
-  			this._lastFetchReturnHeader = fetched.headers;
+				if (!body)
+					throw this.client.logger.error("No Post Type Given in POST fetching");
 
-  			return fetched.json();
-  		}
-  	} catch (e) {
-  		throw this.client.logger.error(e);
-  	}
-  }
+				const fetched = await fetch(`${CONSTANTS.Api}/${options.endpoint}`, {
+					method: options.method,
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bot ${this.client.token}`,
+					},
+					body: body,
+				});
+
+				if (fetched.status === 429) {
+					const json = await fetched.json();
+					this.client.logger.warn(
+						`Rate Limited. Reason: ${json.body}, Global: ${json.global}\n Don't Worry, your request will be retried after ${json.retry_after}`
+					);
+					this._ratelimited += 1;
+					if (this._ratelimited === 50) {
+						this.client.sharder.destroyAll();
+					}
+					await promisify(setTimeout)(json.retry_after);
+					return this.fetch(options);
+				}
+
+				this._lastFetchReturnHeader = fetched.headers;
+
+				return fetched.json();
+			}
+		} catch (e) {
+			throw this.client.logger.error(e);
+		}
+	}
 }
--- src/Utils/EventListener.ts
+++ src/Utils/EventListener.ts
@@ -3,68 +3,73 @@ import { listeners } from "../Decorators/Events";
 import { EVENTS } from "./Constants";
 
 export class EventListener {
-  private _objListeners: Set<Object> = new Set();
-  private _funcListeners = new Objex<(...args: unknown[]) => void, string>();
+	private _objListeners: Set<Object> = new Set();
+	private _funcListeners = new Objex<(...args: unknown[]) => void, string>();
 
-  public addListener(o: Object): void {
-  	this._objListeners.add(o);
-  }
+	public listenerCount(eventName: string) {
+		return this._funcListeners.filter((name: string) => name === eventName)
+			.size;
+	}
 
-  public removeListener(o: Object): void {
-  	this._objListeners.delete(o);
-  }
+	public addListener(o: Object): void {
+		this._objListeners.add(o);
+	}
 
-  public removeAllListeners(): void {
-  	this._funcListeners.clear();
-  	this._objListeners.clear();
-  	listeners.clear();
-  }
+	public removeListener(o: Object): void {
+		this._objListeners.delete(o);
+	}
 
-  public on(name: string, listener: (...args: any[]) => void): void {
-  	this._funcListeners.set(listener, name);
-  }
+	public removeAllListeners(): void {
+		this._funcListeners.clear();
+		this._objListeners.clear();
+		listeners.clear();
+	}
 
-  public off(name: string, listener: (...args: any[]) => void): void {
-  	const value = this._funcListeners.get(listener);
-  	if (value) {
-  		if (value === name) {
-  			this._funcListeners.delete(listener);
-  		}
-  	}
-  }
+	public on(name: string, listener: (...args: any[]) => void): void {
+		this._funcListeners.set(listener, name);
+	}
 
-  public emit(name: EVENTS, ...args: any[]): void {
-  	if (this._objListeners.size !== 0) {
-  		for (const listener of this._objListeners) {
-  			if (Object.keys(listener).includes(name)) {
-  				const func = listener[(name as unknown) as keyof typeof listener];
-  				if (typeof func !== "function") {
-  					throw new TypeError(`${func} should be type of function`);
-  				}
-  				Object.call(listener, func)(...args);
-  			}
-  		}
-  	}
+	public off(name: string, listener: (...args: any[]) => void): void {
+		const value = this._funcListeners.get(listener);
+		if (value) {
+			if (value === name) {
+				this._funcListeners.delete(listener);
+			}
+		}
+	}
 
-  	if (listeners) {
-  		for (const [k, v] of listeners) {
-  			if (k[0] === name) {
-  				try {
-  					const func = v[(k[1] as unknown) as keyof typeof v];
-  					Object.call(v, func)(...args);
-  				} catch (e) {
-  					v.logger.error(e);
-  				}
-  			}
-  		}
-  	}
+	public emit(name: EVENTS, ...args: any[]): void {
+		if (this._objListeners.size !== 0) {
+			for (const listener of this._objListeners) {
+				if (Object.keys(listener).includes(name)) {
+					const func = listener[(name as unknown) as keyof typeof listener];
+					if (typeof func !== "function") {
+						throw new TypeError(`${func} should be type of function`);
+					}
+					Object.call(listener, func)(...args);
+				}
+			}
+		}
 
-  	if (this._funcListeners.size !== 0) {
-  		for (const [key, value] of this._funcListeners) {
-  			if (value == name) {
-  				key(...args);
-  			}
-  		}
-  	}
-  }
+		if (listeners) {
+			for (const [k, v] of listeners) {
+				if (k[0] === name) {
+					try {
+						const func = v[(k[1] as unknown) as keyof typeof v];
+						Object.call(v, func)(...args);
+					} catch (e) {
+						v.logger.error(e);
+					}
+				}
+			}
+		}
+
+		if (this._funcListeners.size !== 0) {
+			for (const [key, value] of this._funcListeners) {
+				if (value == name) {
+					key(...args);
+				}
+			}
+		}
+	}
 }
