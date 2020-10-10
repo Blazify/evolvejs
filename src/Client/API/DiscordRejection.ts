1112e79dd04e0e773747fb228707446ed2b836c6
saberscientist <70531349+saberscientist@users.noreply.github.com>
2020-10-11 02:51:21 +1100

    test

--- src/Client/API/DiscordRejection.ts
+++ src/Client/API/DiscordRejection.ts
@@ -1,9 +1,10 @@
 export default class DiscordRejection extends Error {
-    constructor(struct: { msg: string, code: number, http: number, path: string}) {
+    raw;
+    constructor(struct: { msg: string, code: number, http: number, path: string }) {
         super();
         this.name = "discordRejection"
-        this.message  = `API call rejected with status ${struct?.http}. Message: \u001b[31;1m${struct?.msg}.\u001b[0m Endpoint path: \u001b[31;1m${struct?.path}. Code: ${struct?.code}` 
-
+        this.message = `API call rejected with status ${struct?.http}. Message: \u001b[31;1m${struct?.msg}.\u001b[0m Endpoint path: \u001b[31;1m${struct?.path}. Code: ${struct?.code}`
+        this.raw = struct
     }
 
 }
