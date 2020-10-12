fa9511d8a279fc5ad2a8ecfeaf670dbd53d4a4ce
romeah <runa.ronit3129@gmail.com>
2020-10-12 16:06:11 +0530

    Fix types

--- src/Structures/Message/Message.ts
+++ src/Structures/Message/Message.ts
@@ -23,59 +23,59 @@ export class Message {
   private client!: EvolveClient;
 
   constructor(
-  	data: IMessage,
-  	client: EvolveClient,
-  	channel: TextChannel,
-  	guild?: Guild
+    data: IMessage,
+    client: EvolveClient,
+    channel: TextChannel,
+    guild?: Guild
   ) {
-  	Object.defineProperty(this, "client", {
-  		value: client,
-  		writable: false,
-  		enumerable: false,
-  	});
-  	Object.defineProperty(this, "data", {
-  		value: data,
-  		enumerable: false,
-  		writable: false,
-  	});
-  	if (!this.data) return;
-  	if (this.data.mentions)
-  		for (const it of this.data.mentions) {
-  			this.mentions.push(new User(it));
-  		}
-  	this.channel = channel;
-  	if (guild) this.guild = guild;
-  	if (this.data.guild_id) this.client.rest.getGuild(this.data.guild_id);
-  	this.sentAt = this.data.sent_at;
-  	this.id = this.data.id;
-  	this.pinned = this.data.pinned;
-  	this.rolementions = this.data.mention_roles;
-  	this.mentionEveryone = this.data.mention_everyone;
-  	if (this.data.member) this.member = new GuildMember(this.data.member);
-  	this.author = new User(this.data.author);
-  	if (this.member) this.member.user = this.author;
-  	this.editedTimestamp = this.data.edited_timestamp;
-  	this.attachments = this.data.attachments;
-  	this.content = this.data.content;
-  	return this;
+    Object.defineProperty(this, "client", {
+      value: client,
+      writable: false,
+      enumerable: false,
+    });
+    Object.defineProperty(this, "data", {
+      value: data,
+      enumerable: false,
+      writable: false,
+    });
+    if (!this.data) return;
+    if (this.data.mentions)
+      for (const it of this.data.mentions) {
+        this.mentions.push(new User(it));
+      }
+    this.channel = channel;
+    if (guild) this.guild = guild;
+    if (this.data.guild_id) this.client.rest.getGuild(this.data.guild_id);
+    this.sentAt = this.data.sent_at;
+    this.id = this.data.id;
+    this.pinned = this.data.pinned;
+    this.rolementions = this.data.mention_roles;
+    this.mentionEveryone = this.data.mention_everyone;
+    if (this.data.member) this.member = new GuildMember(this.data.member);
+    this.author = new User(this.data.author);
+    if (this.member) this.member.user = this.author;
+    this.editedTimestamp = this.data.edited_timestamp;
+    this.attachments = this.data.attachments;
+    this.content = this.data.content;
+    return this;
   }
 
-  public delete(time = 0): Promise<NodeJS.Timeout> {
-  	return this.client.rest.deleteMessage(this.id, this.channel.id, time);
+  public delete(time = 0): Promise<void> {
+    return this.client.rest.deleteMessage(this.id, this.channel.id, time);
   }
 
   static async handle(data: IMessage, client: EvolveClient): Promise<Message> {
-  	let message: Message;
-  	let guild: Guild;
-  	const channel: TextChannel = client.channels.get(
-  		data.channel_id
-  	) as TextChannel;
-  	if (data.guild_id) {
-  		guild =
+    let message: Message;
+    let guild: Guild;
+    const channel: TextChannel = client.channels.get(
+      data.channel_id
+    ) as TextChannel;
+    if (data.guild_id) {
+      guild =
         client.guilds.get(data.guild_id) ??
         (await client.rest.getGuild(data.guild_id));
-  		message = new Message(data, client, channel, guild);
-  	} else message = new Message(data, client, channel);
-  	return message;
+      message = new Message(data, client, channel, guild);
+    } else message = new Message(data, client, channel);
+    return message;
   }
 }
